31751c438de92afc237cdde4867cb833d5cd2f1a
jundymek <jundymek@gmail.com>
2016-12-14 23:03:04 +0100

    Wykresy bez responsywności, obliczanie działa ok.

---
+++ bmi_calc/calculation.py
@@ -0,0 +1,46 @@
+
+
+class BmiCalculator():
+
+    def __init__(self, height, weight):
+        self.height = height
+        self.weight = weight
+        self.bmi = weight / ((height / 100)**2)
+
+    def count_bmi(self):
+        return self.bmi
+
+    def bmi_message(self):
+        if self.bmi < 18.5:
+            return('Niedowaga jest wbrew pozorom bardzo niebezpieczna. Może ona\
+                prowadzić do wszelakich zaburzeń funkcjonowania organizmu (np.\
+                pracy serca), zaburzeń hormonalnych, wystąpienia poronień u\
+                kobiet w ciąży, przewlekłego niedoboru witamin i składników\
+                mineralnych, co często wywołuje nieodwracalne zmiany w\
+                organizmie. Przyczynami najczęściej są nadmierne odchudzanie,\
+                anoreksja, bulimia lub często też nowotwór. Objawami, prócz\
+                wychudzenia najczęściej są ospałość, zmniejszona odporność, zła\
+                kondycja włosów i paznokci. Przy niedowadze niekiedy występuje\
+                anemia, osłabienie kości, osteoporoza.', 'niedowaga', 'red',
+                   'not-ok')
+        elif self.bmi >= 18.5 and self.bmi <= 25:
+            return('Świadczy o prawidłowej masie ciała. Oznacza to, że nie ma \
+                potrzeby zmiany stylu życia, czy odżywiania się.', 'waga\
+                prawidłowa', 'green', 'ok')
+        elif self.bmi > 25 and self.bmi <= 30:
+            return('Świadczy o prawidłowej masie ciała. Oznacza to, że nie ma\
+                potrzeby zmiany stylu życia, czy odżywiania się.', 'nadwaga',
+                   'orange')
+        elif self.bmi > 30 and self.bmi <= 35:
+            return('Wynik ten mówi o zwiększonym ryzyku wystąpienia chorób\
+                typowych dla otyłości.', 'otyłość I stopnia', 'red', 'not-ok')
+        elif self.bmi > 35 and self.bmi <= 40:
+            return('W przedziale tym znacząco wzrasta ryzyko wystąpienia chorób\
+                związanych z otyłością.', 'otyłość II stopnia', 'red', 'not-ok')
+        elif self.bmi > 40:
+            return('Osoby, które uzyskały ten wynik powinny jak najszybciej udać\
+                się do lekarza. W tym przedziale występuje bardzo duże zagrożenie\
+                życia. Taką otyłość leczy się specjalnie dobranymi ćwiczeniami,\
+                dietą i lekami. W przypadkach, kiedy to nie wystarczy potrzebna\
+                jest operacja chirurgiczna.', 'otyłość III stopnia', 'red',
+                   'not-ok')
--- bmi_calc/views.py
+++ bmi_calc/views.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Bmi
 from .forms import BmiForm
+from .calculation import BmiCalculator
 
 from graphos.sources.simple import SimpleDataSource
 from graphos.renderers.gchart import ColumnChart
@@ -15,17 +16,24 @@ def index(request):
         if form.is_valid():
             height = form.cleaned_data['height']
             weight = form.cleaned_data['weight']
-            bmi = weight / ((height / 100)**2)
+            wynik = BmiCalculator(height, weight)
+            bmi = wynik.count_bmi()
+            print(bmi)
+            print(int(round(bmi)))
+            message = wynik.bmi_message()
+            # bmi = weight / ((height / 100)**2)
             try:
-                bmi_object = Bmi.objects.get(bmi=int(bmi))
+                bmi_object = Bmi.objects.get(bmi=int(round(bmi)))
                 bmi_object.counter = int(bmi_object.counter) + 1
                 bmi_object.save()
             except ObjectDoesNotExist:
-                bmi_object = Bmi(bmi=int(bmi))
+                bmi_object = Bmi(bmi=int(round(bmi)))
                 bmi_object.save()
-            bmi_wynik = "Twoje BMI wynosi: " + str(int(bmi))
 
-            return render(request, 'bmi_calc/index.html', {'form': form, 'bmi_wynik': bmi_wynik})
+            return render(request, 'bmi_calc/index.html', {'form': form,
+                'bmi': int(round(bmi)), 'message':message[0], 
+                'message1': message[1], 'color': message[2],
+                'ok': message[3],})
     return render(request, 'bmi_calc/index.html', {'form': form})
 
 
